b03c5f53adda4d4d73ec6fa3bab1f8d07c739e5e
TA <TachibanaTsubasa0712@gmail.com>
2018-06-24 19:22:20 +0800

    添加测试用页面

--- login.go
+++ login.go
@@ -6,7 +6,7 @@ import (
 	"text/template"
 )
 
-func renderHtml(w http.ResponseWriter, dir string) error {
+func renderHTML(w http.ResponseWriter, dir string) error {
 	t, err := template.ParseFiles(dir)
 	if err != nil {
 		return err
@@ -15,17 +15,40 @@ func renderHtml(w http.ResponseWriter, dir string) error {
 	return err
 }
 
-func MainHandler(w http.ResponseWriter, r *http.Request) {
-	if err := renderHtml(w, "login.html"); err != nil {
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	if err := renderHTML(w, "search.html"); err != nil {
 		http.Error(w, err.Error(),
 			http.StatusInternalServerError)
 		return
 	}
 }
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	if err := renderHTML(w, "test.html"); err != nil {
+		http.Error(w, err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if err := renderHTML(w, "login.html"); err != nil {
+		http.Error(w, err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/login", http.StatusFound)
+}
+
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", MainHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/search", searchHandler)
+	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/", mainHandler)
 	err := http.ListenAndServe(":7777", nil)
 	if err != nil {
 		log.Fatal("listenAndServe：", err.Error())
